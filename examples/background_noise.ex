ee67c4b2410bea0ed5a38cb43c93935db71618d8
Christopher Bertels <bakkdoor@flasht.de>
2016-11-18 05:17:42 +0100

    Fix example

--- examples/background_noise.ex
+++ examples/background_noise.ex
@@ -16,12 +16,12 @@ defmodule Example.Scenario.BackgroundNoise do
     {:ok, %BackgroundNoise.Session{session: session}}
   end
 
-  def run(bg_session = %{rate: r, spread: s}) do
-    bg_session
+  def run(session) do
+    session
     |> loop(:spread_post_data, 10 |> minutes)
   end
 
-  def spread_post_data(session) do
+  def spread_post_data(session = %{rate: r, spread: s}) do
     session
     |> async_spread(:post_data, r, s)
     |> await(all: :post_data)
