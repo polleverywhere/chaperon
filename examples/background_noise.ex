5d05d802401e44236d06171236104a4063058941
Christopher Bertels <bakkdoor@flasht.de>
2016-11-18 03:21:43 +0100

    Remove typespecs from example for easier readability

--- examples/background_noise.ex
+++ examples/background_noise.ex
@@ -10,22 +10,12 @@ defmodule Example.Scenario.BackgroundNoise do
       spread: 1000, # spread request rate over this amount of milliseconds
       session: nil
     ]
-
-    @type t :: %__MODULE__{
-      rate: non_neg_integer,
-      spread: non_neg_integer,
-      session: Canary.Session.t
-    }
   end
 
-  @type result :: {:ok, BackgroundNoise.Session.t} | {:error, Error.t}
-
-  @spec init(Canary.Session.t) :: result
   def init(session) do
     {:ok, %BackgroundNoise.Session{session: session}}
   end
 
-  @spec run(BackgroundNoise.Session.t) :: result
   def run(bg_session = %{rate: r, spread: s}) do
     bg_session
     |> loop(:spread_post_data, 10 |> minutes)
