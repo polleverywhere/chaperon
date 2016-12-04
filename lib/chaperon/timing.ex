55aa203911d9f0e90f821a9bb88300f330ab569a
Christopher Bertels <bakkdoor@flasht.de>
2016-12-04 20:29:35 +0100

    Add type specs to Timing module

--- lib/chaperon/timing.ex
+++ lib/chaperon/timing.ex
@@ -1,15 +1,24 @@
 defmodule Chaperon.Timing do
+  @type duration :: non_neg_integer
+
   @second 1000
   @minute @second * 60
   @hour   @minute * 60
   @day    @hour * 24
   @week   @day * 7
 
+  @spec seconds(duration) :: duration
   def seconds(num), do: num * @second
-  def minutes(num), do: num * @minute
-  def hours(num), do: num * @hour
-  def days(num), do: num * @day
-  def weeks(num), do: num * @week
 
-  @type duration :: non_neg_integer
+  @spec minutes(duration) :: duration
+  def minutes(num), do: num * @minute
+
+  @spec hours(duration) :: duration
+  def hours(num), do: num * @hour
+
+  @spec days(duration) :: duration
+  def days(num), do: num * @day
+
+  @spec weeks(duration) :: duration
+  def weeks(num), do: num * @week
 end
