8d0c10375918167d93243cd95add850d982d3ed5
Christopher Bertels <bakkdoor@flasht.de>
2017-01-13 23:47:08 -0800

    Add docs to Timing module

--- lib/chaperon/timing.ex
+++ lib/chaperon/timing.ex
@@ -1,4 +1,8 @@
 defmodule Chaperon.Timing do
+  @moduledoc """
+  Timing related helper functions and type definitions used within `Chaperon`.
+  """
+
   @type duration :: non_neg_integer | float
   @type time_unit :: :seconds | :milli_seconds | :micro_seconds | :nano_seconds
 
@@ -8,21 +12,42 @@ defmodule Chaperon.Timing do
   @day    @hour * 24
   @week   @day * 7
 
+  @doc """
+  Returns the correct amount of milliseconds for a given amount of seconds.
+  """
   @spec seconds(duration) :: duration
   def seconds(num), do: round(num * @second)
 
+
+  @doc """
+  Returns the correct amount of milliseconds for a given amount of minutes.
+  """
   @spec minutes(duration) :: duration
   def minutes(num), do: round(num * @minute)
 
+
+  @doc """
+  Returns the correct amount of milliseconds for a given amount of hours.
+  """
   @spec hours(duration) :: duration
   def hours(num), do: round(num * @hour)
 
+  @doc """
+  Returns the correct amount of milliseconds for a given amount of days.
+  """
   @spec days(duration) :: duration
   def days(num), do: round(num * @day)
 
+  @doc """
+  Returns the correct amount of milliseconds for a given amount of weeks.
+  """
   @spec weeks(duration) :: duration
   def weeks(num), do: round(num * @week)
 
+
+  @doc """
+  Returns a timestamp with the given time unit.
+  """
   @spec timestamp(time_unit) :: non_neg_integer
   def timestamp(unit \\ :milli_seconds) do
     :os.system_time(unit)
