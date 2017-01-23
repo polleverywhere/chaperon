132d8622bd59006eb90e8e4cc96a11f07f32e5df
Christopher Bertels <bakkdoor@flasht.de>
2017-01-23 06:16:45 +0100

    Comment out unused generated scenario start_link/1

--- lib/chaperon/scenario.ex
+++ lib/chaperon/scenario.ex
@@ -49,11 +49,11 @@ defmodule Chaperon.Scenario do
       import  Chaperon.Timing
       import  Chaperon.Session
 
-      def start_link(config) do
-        with {:ok, session} <- __MODULE__ |> init(config |> new_session) do
-          Scenario.Task.start_link session
-        end
-      end
+      # def start_link(config) do
+      #   with {:ok, session} <- __MODULE__ |> init(config |> new_session) do
+      #     Chaperon.Scenario.Task.start_link session
+      #   end
+      # end
 
       def new_session(config) do
         %Chaperon.Session{
