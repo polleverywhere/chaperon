0c5d021e96eb14db46296073ddaa32e94fd0e8df
Christopher Bertels <bakkdoor@flasht.de>
2017-08-23 01:32:49 +0200

    Require elixir ~> 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chaperon.Mixfile do
     [
       app: :chaperon,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
