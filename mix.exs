4e590ab12dd8f01f08cc3ec9a2117e2d54cb1adf
Christopher Bertels <bakkdoor@flasht.de>
2017-10-25 10:49:25 +0200

    Release v0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chaperon.Mixfile do
   def project do
     [
       app: :chaperon,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
