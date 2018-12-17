31793c7998dfc3a67348522a18c2c4d0aab6f7ad
Christopher Bertels <bakkdoor@flasht.de>
2018-12-17 18:10:38 +0100

    Update firehose publish roundtrip loadtest to support batch publications

--- examples/firehose_publish_roundtrip.ex
+++ examples/firehose_publish_roundtrip.ex
@@ -3,21 +3,39 @@ defmodule Firehose.Scenario.PublishRountrip do
 
   def run(session) do
     session
+    |> set_config(channel: "#{session.config.channel}-#{:rand.uniform(1_000_000)}")
     |> subscribe
-    |> publish_receive_loop(session.config.iterations)
+    |> publish_receive_loop(publications(session) / batch_size(session))
   end
 
-  def publish_receive_loop(session, 0), do: session
+  defp publications(session) do
+    session |> config(:publications, 500)
+  end
+
+  defp batch_size(session) do
+    session |> config(:batch, 1)
+  end
+
+  def publish_receive_loop(session, iterations) when iterations <= 0 do
+    session
+  end
 
   def publish_receive_loop(session, iterations) do
     session
-    >>> publish_roundtrip
+    |> call_traced(:publish_roundtrip)
     |> publish_receive_loop(iterations - 1)
   end
 
   def publish_roundtrip(session) do
-    session
-    |> publish
+    case session |> batch_size do
+      1 ->
+        session
+        |> publish
+
+      n ->
+        session
+        |> batch_publish(n)
+    end
     |> ws_recv
   end
 
@@ -29,11 +47,25 @@ defmodule Firehose.Scenario.PublishRountrip do
 
   def publish(session) do
     session
-    |> put(session.config.channel, json: json_message())
+    |> put(session.config.channel, json: json_message(session.config.channel, "test"))
   end
 
-  def json_message do
-    %{"hello" => "world", "time" => Chaperon.Timing.timestamp}
+  def batch_publish(session, n) do
+    session
+    |> put("/channels@firehose", json: json_messages(session, n))
+  end
+
+  def json_messages(session, n) do
+    for i <- 0..n, do: json_message(session.config.channel, i)
+  end
+
+  def json_message(channel, msg) do
+    %{
+      channel: channel,
+      payload: %{"hello world" => msg, "time" => Chaperon.Timing.timestamp()},
+      ttl: 1000,
+      buffer_size: 10
+    }
   end
 end
 
@@ -42,17 +74,21 @@ defmodule Firehose.LoadTest.PublishRountrip.Local do
 
   use Chaperon.LoadTest
 
-  def default_config, do: %{
-    scenario_timeout: 25_000,
-    base_url: "http://localhost:7474",
-    channel: "/testchannel"
-  }
+  def default_config,
+    do: %{
+      scenario_timeout: 25_000,
+      base_url: "http://localhost:7474",
+      channel: "/testchannel"
+    }
 
-  def scenarios, do: [
-    {PublishRountrip, %{
-      iterations: 500
-    }}
-  ]
+  def scenarios,
+    do: [
+      {PublishRountrip,
+       %{
+         publications: 500,
+         batch: 10
+       }}
+    ]
 end
 
 Chaperon.run_load_test(Firehose.LoadTest.PublishRountrip.Local)
