96a700a83fae136f4c33cae128a3d685ff95d328
Christopher Bertels <bakkdoor@flasht.de>
2019-06-25 22:43:38 +0200

    Add ExAws & ExAws.S3 dependencies for s3 upload of load test results

--- mix.exs
+++ mix.exs
@@ -91,6 +91,8 @@ defmodule Chaperon.Mixfile do
       {:plug, "~> 1.8"},
       {:plug_cowboy, "~> 2.0"},
       {:basic_auth, "~> 2.2"},
+      {:ex_aws, "~> 2.0"},
+      {:ex_aws_s3, "~> 2.0"},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.14", only: :dev},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
